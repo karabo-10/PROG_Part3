cd719a0ec8dcaa9ed8ac801ef157744dd940316a
karabo-10 <karabokakudi5@gmail.com>
2026-06-25 14:38:54 +0200

    Included data models and basic fiels

--- PROG_Part3/Form1.cs
+++ PROG_Part3/Form1.cs
@@ -7,6 +7,15 @@ using System.Windows.Forms;
 
 namespace PROG_Part3
 {
+    // Data models and basic fiels
+    public class Cybertask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Reminder { get; set; } //e.g . "2023-12-31" or "in 3 days"
+        public bool Completed { get; set; }
+    }
     public partial class Form1 : Form
     {
         public Form1()
